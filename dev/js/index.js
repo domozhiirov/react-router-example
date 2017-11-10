eac61ff42ce16c1e7215331dd3c943c6b37a083b
Vitali <emailme1452@gmail.com>
2017-11-10 15:46:40 -0800

    my commit

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,23 +1,64 @@
-import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from "react-dom";
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
-import createLogger from 'redux-logger';
-import allReducers from './reducers';
-import App from './components/App';
+import React from 'react'
+import {render} from 'react-dom'
 
-const logger = createLogger();
-const store = createStore(
-    allReducers,
-    applyMiddleware(thunk, promise, logger)
-);
+import {Router, Route, Link, browserHistory, IndexRoute, IndexRedirect} from 'react-router'
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+// import io from 'socket.io-client';
+
+// const socket = io('wss://wiki-meta-explorer.herokuapp.com');
+
+// socket.on('connection', (socket) => {
+//   let token = socket.handshake.query.token;
+//
+//   console.log(token);
+// });
+
+const App = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/inbox">Inbox</Link></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
+  }
+})
+
+const About = React.createClass({
+  render() {
+    return <h3>About</h3>
+  }
+})
+
+const Inbox = React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>Inbox</h2>
+        <li><Link to="/inbox/messages/111">Message 111</Link></li>
+        {this.props.children || "Welcome to your Inbox"}
+      </div>
+    )
+  }
+})
+
+const Message = React.createClass({
+  render() {
+    return <h3>Message {this.props.params.id}</h3>
+  }
+})
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="about" component={About} />
+      <Route path="inbox" component={Inbox}>
+        <Route path="messages/:id" component={Message} />
+      </Route>
+    </Route>
+  </Router>
+), document.getElementById('root'))
